a186746c7fea14ff253f03849896342dc63c6f80
ChocoP0ps <alexandre.cano@viacesi.fr>
2017-04-19 20:49:14 +0200

    fin list

--- php/getActivities.php
+++ php/getActivities.php
@@ -1,13 +1,13 @@
 <?php 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdebdd;port=3306, charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=bdebdd;port=3306; charset=utf8', 'root', '');
 
-if($_GET['etat'] == -1)
+if($_POST['etat'] == -1)
 {
-	$stmt = $bdd->query('SELECT * FROM activities');
+	$stmt = $bdd->query('SELECT activities.*, image.PATH_IMAGE FROM activities INNER JOIN photo ON activities.ID_ACTIVITY = photo.ID_ACTIVITY INNER JOIN image ON photo.ID_IMAGE = image.ID_IMAGE WHERE photo.IS_COUVERTURE=1');
 }
 else
 {
-	$stmt = $bdd->query('SELECT * FROM activities WHERE ID_ETAT = ' . $_GET['etat']);
+	$stmt = $bdd->query('SELECT activities.*, image.PATH_IMAGE FROM activities INNER JOIN photo ON activities.ID_ACTIVITY = photo.ID_ACTIVITY INNER JOIN image ON photo.ID_IMAGE = image.ID_IMAGE WHERE photo.IS_COUVERTURE=1 && activities.ID_ETAT=' . $_POST['etat']);
 }
 
 $activity = $stmt->fetchAll(PDO::FETCH_OBJ);

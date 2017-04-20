214e5cfdc681bdf2e8569d708fc8fedf69a2c08d
ChocoP0ps <alexandre.cano@viacesi.fr>
2017-04-20 22:29:43 +0200

    nav

--- activity.php
+++ activity.php
@@ -20,36 +20,36 @@
 </head>
 <body>
 	<header>
-		<div class="container-fluid">	</div>
+		<div class="container-fluid">
+			<h1>BDE Cesi.Exia Aix</h1>
+		</div>
 		<nav class="navbar navbar-default" role="navigation">
-			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="navbar-header">
-				<a class="navbar-brand" href="#">Accueil</a>
+				<a class="navbar-brand" href="index.html">Accueil</a>
 			</div>
-
-			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li class="test"><a href="#">Boutique</a></li>
-					<li><a href="#">Activité</a></li>
-				</ul>
-				<div class="col-sm-3 col-md-3"> 
-					<form class="navbar-form" role="search">
-						<div class="input-group">
-							<input type="text" class="form-control" placeholder="Recherchez une activité" name="q">
-							<div class="input-group-btn">
-								<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+					<li class="shop.html"><a href="shop">Boutique</a></li>
+					<li><a href="form.html"><i class="fa fa-plus-circle" aria-hidden="true"></i>
+						Activité</a></li>
+					</ul>
+					<div class="col-sm-3 col-md-3"> 
+						<form class="navbar-form" role="search">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Recherchez une activité" name="q">
+								<div class="input-group-btn">
+									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</div>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a class="menu" href="contact.html">Contacts</a></li>
+						<li><a href="logreg.php">S'enregistrer / Se connecter</a></li>
+					</ul>
 				</div>
-				<ul class="nav navbar-nav navbar-right">
-					<li><a class="menu" href="#">Contacts</a></li>
-					<li><a href="#">S'enregistrer / Se connecter</a></li>
-				</ul>
-			</div><!-- /.navbar-collapse -->
-		</nav>
-	</header>
+			</nav>
+		</header>
 	<?php
 	if(isset($_GET['idact']))
 	{
